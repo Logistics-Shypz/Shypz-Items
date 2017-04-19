2f837e90af0eff4dad52e88a4df774b7b7faf975
Anubhav Nanda <anubhavnanda@Anubhavs-MacBook-Air.local>
2017-04-19 15:17:20 +0530

    committing in items

---
+++ src/main/java/com/shypz/shypzitems/ShypzItemsApplication.java
@@ -0,0 +1,12 @@
+package com.shypz.shypzitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShypzItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShypzItemsApplication.class, args);
+	}
+}
