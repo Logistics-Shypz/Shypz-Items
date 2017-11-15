8dbbedf5ed1dbd7b6392bfd76753af0547313ee2
Logistics-Shypz <anubhav.nanda@gmail.com>
2017-11-15 20:09:22 +0530

    test

--- src/main/java/com/shypz/shypzitems/ShypzItemsApplication.java
+++ src/main/java/com/shypz/shypzitems/ShypzItemsApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan
 @ComponentScan
+@EnableDiscoveryClient
 public class ShypzItemsApplication {
 
 	public static void main(String[] args) {
